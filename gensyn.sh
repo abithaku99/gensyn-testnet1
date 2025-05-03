6659f211e452192af19f1ff858701434101449a9
abithaku99 <79653682+abithaku99@users.noreply.github.com>
2025-05-03 20:52:23 +0700

    Update gensyn.sh

--- gensyn.sh
+++ gensyn.sh
@@ -6,7 +6,7 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 NC="\e[0m"
 
-SWARM_DIR="$HOME/rl-swarm"
+SWARM_DIR="$HOME/rl-swarm01"
 TEMP_DATA_PATH="$SWARM_DIR/modal-login/temp-data"
 HOME_DIR="$HOME"
 
@@ -29,17 +29,17 @@ if [ -f "$SWARM_DIR/swarm.pem" ]; then
             rm -rf "$SWARM_DIR"
 
             echo -e "${BOLD}${YELLOW}[✓] Cloning fresh repository...${NC}"
-            cd $HOME && git clone https://github.com/zunxbt/rl-swarm.git > /dev/null 2>&1
+            cd $HOME && git clone https://github.com/abithaku99/rl-swarm01.git > /dev/null 2>&1
 
-            mv "$HOME_DIR/swarm.pem" rl-swarm/
-            mv "$HOME_DIR/userData.json" rl-swarm/modal-login/temp-data/ 2>/dev/null
-            mv "$HOME_DIR/userApiKey.json" rl-swarm/modal-login/temp-data/ 2>/dev/null
+            mv "$HOME_DIR/swarm.pem" rl-swarm01/
+            mv "$HOME_DIR/userData.json" rl-swarm01/modal-login/temp-data/ 2>/dev/null
+            mv "$HOME_DIR/userApiKey.json" rl-swarm01/modal-login/temp-data/ 2>/dev/null
             break
         elif [ "$choice" == "2" ]; then
             echo -e "${BOLD}${YELLOW}[✓] Removing existing folder and starting fresh...${NC}"
             rm -rf "$SWARM_DIR"
             sleep 2
-            cd $HOME && git clone https://github.com/zunxbt/rl-swarm.git > /dev/null 2>&1
+            cd $HOME && git clone https://github.com/abithaku99/rl-swarm01.git > /dev/null 2>&1
             break
         else
             echo -e "\n${BOLD}${RED}[✗] Invalid choice. Please enter 1 or 2.${NC}"
@@ -47,10 +47,10 @@ if [ -f "$SWARM_DIR/swarm.pem" ]; then
     done
 else
     echo -e "${BOLD}${YELLOW}[✓] No existing swarm.pem found. Cloning repository...${NC}"
-    cd $HOME && [ -d rl-swarm ] && rm -rf rl-swarm; git clone https://github.com/abithaku99/rl-swarm01.git > /dev/null 2>&1
+    cd $HOME && [ -d rl-swarm01 ] && rm -rf rl-swarm01; git clone https://github.com/abithaku99/rl-swarm01.git > /dev/null 2>&1
 fi
 
-cd rl-swarm || { echo -e "${BOLD}${RED}[✗] Failed to enter rl-swarm directory. Exiting.${NC}"; exit 1; }
+cd rl-swarm01 || { echo -e "${BOLD}${RED}[✗] Failed to enter rl-swarm01 directory. Exiting.${NC}"; exit 1; }
 
 echo -e "${BOLD}${YELLOW}[✓] Running rl-swarm...${NC}"
 ./run_rl_swarm.sh
